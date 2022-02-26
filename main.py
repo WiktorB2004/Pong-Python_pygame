3b390ef975203b9714cbb17bf14cbd4d906da1ea
WiktorB2004 <byrka.w@gmail.com>
2022-02-26 23:11:00 +0100

    Initialized paddles and their movement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # module imports
+from email.mime import image
 import pygame
 import os
 # pygame initializations
@@ -14,10 +15,19 @@ BORDER = pygame.Rect(WIDTH//2 - 2, 0, 4, HEIGHT)
 # Color variables
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
+
+# Paddle variables
+YELLOW_PADDLE = pygame.transform.rotate(pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'yellow_paddle.png')), (100, 20)), 90)
+BLUE_PADDLE = pygame.transform.rotate(pygame.transform.scale(pygame.image.load(os.path.join('Assets', 'blue_paddle.png')), (100, 20)), 90)
+paddle_vel = 5
+
 # Classes
 class Paddle():
-    pass
-
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.lives = 3
+        
 class Ball():
     pass
 
@@ -26,9 +36,12 @@ def main():
     run = True
     FPS = 60
     clock = pygame.time.Clock()
-    
+    blue_paddle = Paddle(5, HEIGHT//2 - BLUE_PADDLE.get_height()//2)
+    yellow_paddle = Paddle(WIDTH - YELLOW_PADDLE.get_width() - 5, HEIGHT//2 - YELLOW_PADDLE.get_height()//2)
     def redraw_window(window):
         window.blit(BG, (0, 0))
+        WIN.blit(BLUE_PADDLE, (blue_paddle.x, blue_paddle.y))
+        WIN.blit(YELLOW_PADDLE, (yellow_paddle.x, yellow_paddle.y))
         pygame.draw.rect(WIN, WHITE, BORDER)
         pygame.display.update()
     
@@ -38,6 +51,19 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 quit()
+                
+        pressed_keys = pygame.key.get_pressed()
+        # BLUE PADDLE MOVEMENT
+        if pressed_keys[pygame.K_w] and blue_paddle.y + paddle_vel > 0: # UP
+            blue_paddle.y -= paddle_vel
+        if pressed_keys[pygame.K_s] and blue_paddle.y + BLUE_PADDLE.get_height() + paddle_vel < HEIGHT: # DOWN
+            blue_paddle.y += paddle_vel
+        # YELLOW PADDLE MOVEMENT
+        if pressed_keys[pygame.K_UP] and yellow_paddle.y + paddle_vel > 0: # UP
+            yellow_paddle.y -= paddle_vel
+        if pressed_keys[pygame.K_DOWN] and yellow_paddle.y + YELLOW_PADDLE.get_height() + paddle_vel < HEIGHT: # DOWN
+            yellow_paddle.y += paddle_vel
+            
 
 # Main menu
 def main_menu():
